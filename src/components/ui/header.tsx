91f2ea438da779b1351c4771030bd6f9a2121f92
patricklimax <patricklimax@outlook.com.br>
2023-10-23 12:14:17 -0300

    feat: add header menu sheet components

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,13 +1,58 @@
-import { MenuIcon, ShoppingCartIcon } from 'lucide-react';
+import { HomeIcon, ListOrderedIcon, LogInIcon, LogOutIcon, MenuIcon, PercentIcon, ShoppingCartIcon } from 'lucide-react';
 import { Button } from './button';
 import { Card } from './card';
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from './sheet';
 
 const Header = () => {
   return ( 
     <Card className='flex items-center justify-between p-4'>
-      <Button size='icon' variant='outline' className='rounded'>
-        <MenuIcon />
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size='icon' variant='outline' className='rounded'>
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+
+        <SheetContent side='left'>
+          <SheetHeader className='text-left text-lg font-semibold'>
+            Menu
+          </SheetHeader>
+
+          <div className='py-4 flex flex-col gap-4'>
+            <Button variant='outline' className='w-full justify-start rounded gap-2 items-center'>
+              <LogInIcon size={16}/>
+              Fazer login
+            </Button>
+
+            <Button variant='outline' className='w-full justify-start rounded gap-2 items-center'>
+              <HomeIcon size={16} />
+              Home
+            </Button>
+
+            <Button variant='outline' className='w-full justify-start rounded gap-2 items-center'>
+              <PercentIcon size={16} />
+              Ofertas
+            </Button>
+
+            <Button variant='outline' className='w-full justify-start rounded gap-2 items-center'>
+              <LogOutIcon size={16} />
+              Fazer Logout
+            </Button>
+
+            <Button variant='outline' className='w-full justify-start rounded gap-2 items-center'>
+              <ListOrderedIcon size={16} />
+              Catálogo
+            </Button>
+
+          </div>
+
+
+          
+
+        </SheetContent>
+      </Sheet>
+
+      
 
       <h1 className='font-semibold text-xl'>
         <span className='text-primary'>Developer</span> Store
