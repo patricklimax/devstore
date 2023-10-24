a328ca93530c07f1c24dce643e2013ed8d042882
patricklimax <patricklimax@outlook.com.br>
2023-10-23 22:57:32 -0300

    feat: add Categories and CategoriesItem components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,24 @@
 "use client"
 
 import Image from 'next/image'
+import Categories from './components/categories'
 
 export default function Home() {
 
   return (
     <main>
-      <Image
-        src={'/bannerHome01.svg'}
-        alt={'Até 55% de desconto esse mês!'}
-        height={0}
-        width={0}
-        sizes='100vw'
-        className='h-auto w-full'
-      />
+      <div className='flex flex-col md:flex-row gap-4 py-4'>
+        <Categories />
+        <Image
+          src={'/bannerHome01.svg'}
+          alt={'Até 55% de desconto esse mês!'}
+          height={0}
+          width={0}
+          sizes='100vw'
+          className='h-auto w-full'
+        />
+      </div>
+
     </main>
   )
 }
